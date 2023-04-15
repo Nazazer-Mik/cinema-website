ba92bb097cf03408aa6ff1dece0994927dacc5fb
Nazar Mikhailychenko <nazar.mikhailichenko05@gmail.com>
2023-04-15 23:58:37 +0100

    First Movie added

--- src/components/MovieBlock/MovieBlock.js
+++ src/components/MovieBlock/MovieBlock.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './MovieBlock.css'
 
-class MovieBlock extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+function MovieBlock (props) {
+
+    let movieBlock = {
+        backgroundImage: 'url(' + props.imgPath +')',
+        height: window.innerHeight/2.5,
+        width: (window.innerWidth*0.88)/3
     }
+
+    return (
+        <div className='movie-block' style={movieBlock}>
+            <div className='wrap'>
+                <div className='description-block'>
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default MovieBlock;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import Main from '../../components/Main/Main.js'
+import React from 'react';
+import './Home.css'
+import MovieBlock from '../../components/MovieBlock/MovieBlock.js'
 
-class Home extends Component {
-    render() {
-        return (
-            <>
-                <Main />
-            </>
-        );
-    }
+function Home () {
+    return (
+        <div className='main-block'>
+            <MovieBlock imgPath={'./images/movies/puss-in-boots.jpg'} title={'Puss in Boots'} description={'When Puss in Boots discovers that his passion for adventure has taken its toll and he has burned through eight of his nine lives, he launches an epic journey to restore them by finding the mythical Last Wish.'}/>
+        </div>
+    );
 }
 
 export default Home;

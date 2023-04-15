b84c3bd24a3a6bbce8e516f0ae3daa6bec909ccf
Nazar Mikhailychenko <nazar.mikhailichenko05@gmail.com>
2023-04-15 21:32:52 +0100

    Footer edited

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './Main.css'
+import MovieBlock from '../../components/MovieBlock/MovieBlock.js'
 
 class Main extends Component {
     render() {
         return (
             <div className='main-block'>
-                Wtf
+                <MovieBlock />
             </div>
         );
     }
---
+++ src/components/MovieBlock/MovieBlock.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieBlock extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MovieBlock;
